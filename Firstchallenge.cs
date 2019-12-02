a2cefd1d47d249ade592ce241945805955e2c6be
skliz4rel <skliz4rel@yahoo.com>
2019-12-02 15:14:33 -0800

    First commit
    This is my first commit

---
+++ Firstchallenge.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datastructure
+{
+    public class Firstchallenge
+    {
+
+        public Firstchallenge()
+        {
+
+        }
+
+        public static void Showmap()
+        {
+            Dictionary<string, string> amap = new Dictionary<string, string>();
+
+            amap.Add("1","rice");
+            amap.Add("2","beans");
+            amap.Add("3","yam");
+            amap.Add("4","semo");
+
+            foreach (var item in amap)
+            {
+                Console.WriteLine(item.Value);
+            }
+        }
+
+    }
+}
